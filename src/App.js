de884b3f6d58896818134146a49232276ecbe62a
nani kim <flsk9698@naver.com>
2024-03-27 22:48:11 +0900

    project set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetails from './page/ProductDetails';
+
+//1. 전체상품, 로그인, 상품 상세 페이지
+//2. 전체 상품 페이지에서는 전체 상품을 볼 수 있다.
+//3. 로그인 버튼을 누르면 로그인 페이지 호출
+//4. 상품 디테일을 눌렀을때, 로그인이 되어 있지 않다면 로그인 페이지 호출
+//5. 로그인이 되어 있다면, 상품 디테일 페이지 호출
+//6. 로그아웃버튼을 클릭하면, 로그아웃 처리
+//7. 로그아웃 되면 상품 디테일 페이지를 볼 수 없다. 다시 로그인 해야함.
+//8. 로그인/로그아웃 (토글)
+//9. 상품 검색
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<ProductAll/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/product/:id" element={<ProductDetails/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/page/ProductDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <h1>상품 상세 페이지</h1>
+    </div>
+  )
+}
+
+export default ProductDetails
